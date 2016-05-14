e6dd26d97421981b12c2fb105c9ab57a8a3eeb74
nobodykid <muhamadiqbal@live.co.uk>
2016-05-14 13:41:35 +0700

    Initial commit

---
+++ HelloMVC/HelloMVC.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HelloMVC.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
