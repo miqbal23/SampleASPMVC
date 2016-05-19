a7b30fae4d03237c448a63fb7718ab16697d87d6
nobodykid <muhamadiqbal@live.co.uk>
2016-05-19 15:20:11 +0700

    created Model and Controller

---
+++ HelloMVC/HelloMVC/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HelloMVC.Models;
+
+namespace HelloMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            List<Person> _person = new List<Person>()
+            {
+                new Person { Name = "Muhamad Iqbal", Country = "Depok" }
+            };
+            return View(_person);
+        }
+    }
+}
